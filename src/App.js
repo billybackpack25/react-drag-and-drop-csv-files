efd0a04eca846a9533f86101a9dca78b7ee1d93a
billybackpack25@gmail.com <billybackpack25@gmail.com>
2021-08-29 21:46:38 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { parse } from 'papaparse';
 
-function App() {
+export default function App() {
+  const [highlighted, setHighlighted] = React.useState(false);
+  const [contacts, setContacts] = React.useState([
+    {email:"fake@gmail.com",name:'fake'},
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <h1 className="text-center text-4xl">Contact Import</h1>
+      <div className={`p-6 my-2 mx-auto max-w-md border-2 ${
+        highlighted ? 'border-green-600 bg-green-100' : 'border-gray-600'
+        }`}
+        onDragEnter={() => {
+          setHighlighted(true);
+        }}
+        onDragLeave={() => {
+          setHighlighted(false);
+        }}
+        onDragOver={(e) => {
+          // Prevent the browser from opening up / downloading the file 
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          // Prevent the browser from opening up / downloading the file 
+          e.preventDefault();
+          setHighlighted(false);
+          // Print out what the browser catches when a file is dropped
+          // console.log(e.dataTransfer.files)
+          /* 
+            lastModified: 1627043717652
+            lastModifiedDate: Fri Jul 23 2021 13:35:17 GMT+0100 (British Summer Time) {}
+            name: "Screenshot 2021-07-23 133517.png"
+            size: 143531
+            type: "image/png"
+            webkitRelativePath: ""
+          */
+          // Convert dataObject to array, where filetype is csv
+          Array.from(e.dataTransfer.files)
+            .filter(file => file.type === 'application/vnd.ms-excel')
+            .forEach(async (file) => {
+            // Do something with the files here
+            const text = await file.text();
+            const result = parse(text, { header: true });
+            //console.log(result)
+            setContacts(prev => [...prev, ...result.data])
+          })
 
-export default App;
+        }}
+      >
+        DROP HERE
+      </div>
+      <ul>
+        {
+          contacts.map(contact => (
+            <li key={contact.email}>
+              <strong>{contact.name}</strong>: {contact.email}
+            </li>
+            )
+          )
+        }
+      </ul>
+    </div>
+  )
+}
